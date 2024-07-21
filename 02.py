dbd3f9d053cdb8cf6240041090be929c96397add
zhoujingya <jing.zhou@terapines.com>
2024-07-21 09:52:45 +0800

    Using class to wrapper jit

--- 02.py
+++ 02.py
@@ -1,12 +1,17 @@
 import inspect
 import ast
 
-def jit(fn):
-    def inner():
-        fn_src = inspect.getsource(fn)
+class JIT:
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __call__(self, *args, **kwars):
+        fn_src = inspect.getsource(self.fn)
         fn_ast = ast.parse(fn_src)
         print(ast.dump(fn_ast))
-    return inner
+
+def jit(fn):
+    return JIT(fn)
 
 @jit
 def add():
